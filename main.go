fd49eea210a46af9eb17c8de3e396427a257a2ce
John Emrich <jpemrich@gmail.com>
2019-02-06 07:32:46 -0500

    Added some sample code for regular expressions

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"regexp"
 
 	"github.com/pborman/getopt/v2"
 )
@@ -29,4 +30,15 @@ func main() {
 	if *cmdFlag != "" {
 		fmt.Printf("Running command %s", *cmdFlag)
 	}
+	// Testing regular expressions
+	r := regexp.MustCompile(`(?P<Year>\d{4})-(?P<Month>\d{2})-(?P<Day>\d{2})`)
+	fmt.Printf("%#v\n", r.FindStringSubmatch(`2015-05-27`))
+	fmt.Printf("%#v\n", r.SubexpNames())
+	// From docs
+	re := regexp.MustCompile("(?P<first>[a-zA-Z]+) (?P<last>[a-zA-Z]+)")
+	fmt.Println(re.MatchString("Alan Turing"))
+	fmt.Printf("%q\n", re.SubexpNames())
+	reversed := fmt.Sprintf("${%s} ${%s}", re.SubexpNames()[2], re.SubexpNames()[1])
+	fmt.Println(reversed)
+	fmt.Println(re.ReplaceAllString("Alan Turing", reversed))
 }
